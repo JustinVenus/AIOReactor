7cdee38190eda79766ab3342ef285038bb62a1d2
Justin Venus <justin.venus@gmail.com>
2012-04-02 19:56:14 -0500

    checking in first pass

---
+++ setup.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2012 Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+
+"""
+Distutils file for building low-level AIO bindings from their Pyrex source
+"""
+
+# since this is for solaris you will need to use the sunpro/solarisstudio
+# compiler to build and link this module.
+#
+# example invocation
+#
+#   CC=/opt/solarisstudio/bin/cc python setup.py build
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [
+        Extension("_aio", 
+            ["aio.pyx"],
+            libraries=["aio"]
+        )
+    ]
+)
